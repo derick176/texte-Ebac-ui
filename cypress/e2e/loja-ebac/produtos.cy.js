ac36dcae25d5453addccbd6a62d0ff5050f9d61e
Derick filipe <derickfilipe11@gmail.comm>
2025-10-28 13:11:47 -0300

    slecionar produtos

---
+++ cypress/e2e/loja-ebac/produtos.cy.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress"/>
+describe('funcionalidade: produtos', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+    
+    it('deve selecionar um produto da lista', () => {
+        cy.get(' .product-block ')
+            //.first()
+            //.last()
+            //.eq(2)
+            .contains('Argus All-Weather Tank')
+            .click()
+
+            cy.get('#tab-title-description > a').should('contain' , 'Descrição')
+
+    });
+});
